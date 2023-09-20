0365866d2176fbc43e4f379e364298188bb81c5c
Mario Daza <marioivandaza25@gmail.com>
2023-09-20 15:53:31 -0500

    feat(Entities): :rocket: Creacion Entidad UserRole
    Creacion de la entidad UserRole y su respectivo Icollection en Jobtitle

--- Domain/Entities/JobTitle.cs
+++ Domain/Entities/JobTitle.cs
@@ -4,4 +4,5 @@ public class JobTitle : BaseEntity
 {
     public string Description { get; set; }
     public ICollection<Person> People { get; set; }
+    public ICollection<UserRole> UsersRoles { get; set; }
 }
