b5d89219be5bc6c759b46a5d7bfaf63690971006
Nixon Amado <86610615+NixonAmado@users.noreply.github.com>
2023-09-21 19:08:58 +0000

    feat(ApiPharmacy): :package: creacion de la carpeta Helpers y JWT
    creacion del archivo JWT que esta contenido en la carpeta helpers

---
+++ ApiPharmacy/Helpers/JWT.cs
@@ -0,0 +1,10 @@
+namespace ApiPharmacy.Helpers
+{
+    public class JWT
+    {
+        public string Key {get;set;}
+        public string Issuer {get;set;}
+        public string Audience {get;set;}
+        public DateTime DurationInMinutes {get;set;}
+    }
+}
