2095312dc60d6de8f55a7d075eb12187000f400d
Mario Daza <marioivandaza25@gmail.com>
2023-09-21 01:51:01 -0500

    feat(ApiPharmacy): :rocket: AddDbContext en Program

--- ApiPharmacy/Program.cs
+++ ApiPharmacy/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First()); });
 
+builder.Services.AddDbContext<PharmacyContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
