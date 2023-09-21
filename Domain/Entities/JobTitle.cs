65dfc20d5bfeff96aedf06d50c1360329a625b42
Mario Daza <marioivandaza25@gmail.com>
2023-09-21 03:54:33 -0500

    refactor(Entities): :construction: Cambios para mejora de Entidades

--- Domain/Entities/JobTitle.cs
+++ Domain/Entities/JobTitle.cs
@@ -4,5 +4,6 @@ public class JobTitle : BaseEntity
 {
     public string Description { get; set; }
     public ICollection<Person> People { get; set; }
+    public ICollection<User> Users { get; set; } = new HashSet<User>();
     public ICollection<UserRole> UsersRole { get; set; }
 }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,6 +7,7 @@ public class User : BaseEntity
     public string Email { get; set; }
     public int  Employee_Fk { get; set; }
     public Person Employee { get; set; }
-    public ICollection<RefreshToken> RefreshTokens { get; set; }
+    public ICollection<JobTitle> JobsTitle { get; set; } = new HashSet<JobTitle>();
     public ICollection<UserRole> UsersRole { get; set; }
+    public ICollection<RefreshToken> RefreshTokens { get; set; }
 }
