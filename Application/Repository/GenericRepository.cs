6562ff0d8ce9f4ceb001254d8df09dd9da302795
Mario Daza <marioivandaza25@gmail.com>
2023-09-21 03:53:36 -0500

    feat(Repository): :construction: Creacion de Repositorios

---
+++ Application/Repository/GenericRepository.cs
@@ -0,0 +1,56 @@
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Repository;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly PharmacyContext _context;
+
+    public GenericRepository(PharmacyContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+
+    public virtual IEnumerable<T> Find(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public virtual void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+}
---
+++ Application/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUser
+    {
+        private readonly PharmacyContext _context;
+        public UserRepository(PharmacyContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _context.Users
+                                .Include(u=>u.JobsTitle)
+                                .FirstOrDefaultAsync(u=>u.Username.ToLower()==username.ToLower());
+        }
+        public async Task<User> GetByRefreshTokenAsync(string refreshToken)
+        {
+            return await _context.Users
+                .Include(u => u.JobsTitle)
+                .Include(u => u.RefreshTokens)
+                .FirstOrDefaultAsync(u => u.RefreshTokens.Any(t => t.Token == refreshToken));
+        }
+    }
+}
