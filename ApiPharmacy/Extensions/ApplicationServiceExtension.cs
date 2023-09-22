be0d71f7860bfd6393a0798f1ed9fcb104a57c1c
Giovanny Moreno <giovannysocas@gmail.com>
2023-09-21 22:22:40 -0500

    feat: Create helpers Authorization

--- ApiPharmacy/Extensions/ApplicationServiceExtension.cs
+++ ApiPharmacy/Extensions/ApplicationServiceExtension.cs
@@ -21,4 +21,33 @@ public static class ApplicationServiceExtension
             services.AddScoped<IUnitOfWork,UnitOfWork>();
             services.AddScoped<IPasswordHasher<User>,PasswordHasher<User>>();
         }
+
+     public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        //Configuration from AppSettings
+        services.Configure<JWT>(configuration.GetSection("JWT"));
+
+        //Adding Athentication - JWT
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(o =>
+            {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = false;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
+                };
+            });
+    }
 }
