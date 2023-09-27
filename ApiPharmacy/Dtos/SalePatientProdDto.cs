7ccd8f672b8f47607d8925f6ca9195743c054fa8
Mario Daza <marioivandaza25@gmail.com>
2023-09-27 15:42:02 -0500

    feat: :zap: General

--- ApiPharmacy/Dtos/SalePatientProdDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ApiPharmacy.Dtos;
-
-public class SalePatientProdDto
-{
-    public int Id { get; set; }
-    public string Identification { get; set; }
-    public string Name { get; set; }
-}
--- Domain/Entities/IdentificationType.cs
+++ Domain/Entities/IdentificationType.cs
@@ -1,7 +1,10 @@
+using System.Xml.Serialization;
+
 namespace Domain.Entities;
 
 public class IdentificationType : BaseEntity
 {
     public string Description { get; set; }
+    [XmlIgnore]
     public ICollection<Person> People { get; set; }
 }
