4bd4ee7cb7f20becd4787af8df978a4d2df4faec
Mario Daza <marioivandaza25@gmail.com>
2023-09-27 15:34:52 -0500

    feat: :zap: Implementacion EndPoints Recetas

--- ApiPharmacy/Dtos/RecipeDto.cs
+++ ApiPharmacy/Dtos/RecipeDto.cs
@@ -4,6 +4,7 @@ public class RecipeDto
 {
     public int Id { get; set; }
     public DateTime CreateDate { get; set; }
-    public int Doctor_Fk { get; set; }
-    public int Patient_Fk { get; set; }
+    public DoctorDto Doctor { get; set; }
+    public PatientRecipeDto Patient { get; set; }
+    public List<ProductRecipeDto> RecipeProducts { get; set; }
 }
