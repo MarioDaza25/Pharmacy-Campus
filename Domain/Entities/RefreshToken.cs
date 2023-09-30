2ad8c1573c4115c5bce7ba3a22a1c2aab759aeab
nixonamado <nixonamado00@gmail.com>
2023-09-30 06:00:05 -0500

    fix: :bug: un signo puede joder el JWT
    cambios en bugs de JWT

--- Domain/Entities/RefreshToken.cs
+++ Domain/Entities/RefreshToken.cs
@@ -8,6 +8,6 @@ public class RefreshToken : BaseEntity
     public DateTime Expires { get; set; }
     public bool IsExpired => DateTime.UtcNow >= Expires;
     public DateTime Created { get; set; }
-    public DateTime? Revoked { get; set; }
+    public DateTime Revoked { get; set; }
     public bool IsActive => Revoked == null && !IsExpired;
 }
