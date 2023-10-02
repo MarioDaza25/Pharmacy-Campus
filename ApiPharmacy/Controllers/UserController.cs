0905da1473ebb02a555c2f612e3093c5dc8db957
nixonamado <nixonamado00@gmail.com>
2023-10-02 10:31:40 -0500

    feat: :art: Toques finales
    creacion de refreshtoken para manipularlo en el front

--- ApiPharmacy/Controllers/UserController.cs
+++ ApiPharmacy/Controllers/UserController.cs
@@ -36,5 +36,25 @@ namespace ApiPharmacy.Controllers
             var result = await _userService.AddRoleAsync(model);
             return Ok(result);
         }
+        [HttpPost("refresh-token")]
+    public async Task<IActionResult> RefreshToken()
+    {
+        var refreshToken = Request.Cookies["refreshToken"];
+        var response = await _userService.RefreshTokenAsync(refreshToken);
+        if (!string.IsNullOrEmpty(response.RefreshToken))
+            SetRefreshTokenInCookie(response.RefreshToken);
+        return Ok(response);
+    }
+
+
+    private void SetRefreshTokenInCookie(string refreshToken)
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Expires = DateTime.UtcNow.AddDays(10),
+        };
+        Response.Cookies.Append("refreshToken", refreshToken, cookieOptions);
+    }
     }
 }
