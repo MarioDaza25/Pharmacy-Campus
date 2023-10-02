88b0d7e474c1fa753d503631f6361dfcd7fa51f1
nixonamado <nixonamado00@gmail.com>
2023-10-02 16:22:44 -0500

    feat: PaginacionVersionado
    creacion de paginacion y versionaado

--- Application/Repository/GenericRepository.cs
+++ Application/Repository/GenericRepository.cs
@@ -58,4 +58,13 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
         _context.Set<T>().Update(entity);
     }
+     public virtual async Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync (int pageIndex, int pageSize, string _search)
+    {
+        var totalRegistros = await _context.Set<T>().CountAsync();
+        var registros = await _context.Set<T>()
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        return (totalRegistros, registros);
+    }
 }
