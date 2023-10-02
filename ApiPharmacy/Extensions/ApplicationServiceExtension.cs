c11832ddbf27134b6b4c98716c9df6b2ba7eb97e
nixonamado <nixonamado00@gmail.com>
2023-10-02 15:19:33 -0500

    feat:

--- ApiPharmacy/Extensions/ApplicationServiceExtension.cs
+++ ApiPharmacy/Extensions/ApplicationServiceExtension.cs
@@ -8,6 +8,9 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using ApiPharmacy.Services;
 using API.Services;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 namespace ApiPharmacy.Extensions;
 public static class ApplicationServiceExtension
 {
@@ -26,6 +29,42 @@ public static class ApplicationServiceExtension
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
+    public static void ConfigureRateLimiting(this IServiceCollection services) 
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddInMemoryRateLimiting();
+            services.Configure<IpRateLimitOptions>(options =>
+            {
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = false;
+                options.HttpStatusCode = 429;
+                options.RealIpHeader = "X-Real-IP";
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint = "*",
+                        Period = "10s",
+                        Limit = 3
+                    }
+                };
+
+            });
+        }
+        public static void ConfigureApiVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1,0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("ver"),
+                    new HeaderApiVersionReader("X-Version")
+                );
+            
+            });
+        }
     public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
 
     {
