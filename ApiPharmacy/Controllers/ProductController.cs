ca60c11dd53d4664b2f79a21279e2f6bd695ac5c
nixonamado <nixonamado00@gmail.com>
2023-10-02 22:05:08 -0500

    feat(ApiPharmacy): :sparkles: Creacion de autorizaciones
    restringue quien puede consumir los endpoints, ya lo probé, y funciona

--- ApiPharmacy/Controllers/ProductController.cs
+++ ApiPharmacy/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiPharmacy.Controllers;
     [ApiVersion("1.0")]
     [ApiVersion("1.1")]
-    [Authorize(Roles = "Cajero")]
+    [Authorize(Roles = "Adimistrador , gerente")]
 public class ProductController : BaseApiController
 {    
         private readonly IUnitOfWork _unitOfWork;
@@ -23,6 +23,8 @@ public class ProductController : BaseApiController
 
     //Listar Todos Los Productos
     [HttpGet]
+    [AllowAnonymous]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<IEnumerable<ProductDto>>> Get()
@@ -32,7 +34,9 @@ public class ProductController : BaseApiController
     }
 
     [HttpGet]
+    [AllowAnonymous]
     [MapToApiVersion("1.1")]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Pager<ProductDto>>> Get11([FromQuery] Params productParams)
@@ -253,6 +257,8 @@ public class ProductController : BaseApiController
 
 
     [HttpGet("{id}")]
+    [AllowAnonymous]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Get(int id)
@@ -262,6 +268,8 @@ public class ProductController : BaseApiController
     }
 
     [HttpPost]
+    [AllowAnonymous]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<Product>> Post(ProductDto productDto)
@@ -279,6 +287,8 @@ public class ProductController : BaseApiController
 
 
     [HttpPut("{id}")]
+    [AllowAnonymous]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<ActionResult<ProductDto>> Put(int id, [FromBody] ProductDto productDto)
     {
@@ -294,6 +304,8 @@ public class ProductController : BaseApiController
     }
 
     [HttpDelete("{id}")]
+    [AllowAnonymous]
+    [Authorize(Roles = "Cajero")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Delete(int id)
@@ -309,7 +321,4 @@ public class ProductController : BaseApiController
 
         return NoContent();
     }
-
-
-
 }
